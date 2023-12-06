d841f8d2a2647db70e5e53cc8b00333809c787e4
Elisha <elishakibet67@gmail.com>
2023-12-06 08:20:34 +0300

    Update person.py

--- lib/person.py
+++ lib/person.py
@@ -2,13 +2,16 @@
 
 class Person:
     # Class body goes here
+    def __init__(self, name):
+        self.name = name
+
     #Instance method definition
     def talk(self):
         print("Hello World!")
 
     def walk(self):
-        print("The person is walking")
+        print(f"{self.name} is walking")
 
-elisha = Person()
+elisha = Person("Elisha")
 elisha.talk()
 elisha.walk()
